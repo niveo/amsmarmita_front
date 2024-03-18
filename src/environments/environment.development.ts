fcab88b04c0123391336a135e8b20ec50b164814
amsraimar <amsraimar@gmail.com>
2024-03-18 18:01:24 -0300

    Vinculo prato e pedido com store

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,6 +3,6 @@ import version from './version';
 export const environment = {
   titulo: 'Clube da Marmita',
   production: false,
-  apiUri: 'http://172.23.81.123:3000',
+  apiUri: 'http://192.168.0.129:3000',
   ...version,
 };
