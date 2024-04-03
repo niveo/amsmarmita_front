0dff2d7cf3a6172a7d3db0b57132e779f4dfadaf
amsraimar <amsraimar@gmail.com>
2024-04-03 11:47:47 -0300

    Notificando sessão finalizada

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -8,41 +8,42 @@ import { LBL_ERRO } from '../../common/constantes';
   templateUrl: './login.component.html',
   styles: [
     `
+      .content {
+        justify-content: center;
+        padding: 10px;
+        display: grid;
+        grid-template-columns: 80px 80px 80px;
+        gap: 10px;
+      }
 
-  .content {
-     justify-content: center;
-     padding: 10px;
-     display: grid;
-     grid-template-columns: 80px 80px 80px; gap: 10px;
-  }
+      .box0 {
+        grid-column-start: 1;
+        grid-column-end: 2;
+        grid-row-start: 0;
+      }
 
-  .box0 {
-    grid-column-start: 1;
-    grid-column-end: 2;
-    grid-row-start: 0;
-  }
+      .box1 {
+        grid-column-start: 2;
+        grid-column-end: 4;
+        grid-row-start: 0;
+      }
 
-  .box1 {
-    grid-column-start: 2;
-    grid-column-end: 4;
-    grid-row-start: 0;
-  }
-
-  :host {
-    margin: auto;
-    //position: relative;
-    top: 100px;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    background-color: white;
-  }
-`,
+      :host {
+        margin: auto;
+        //position: relative;
+        top: 100px;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        background-color: white;
+      }
+    `,
   ],
 })
 export class LoginComponent {
   quantidadeNumeros: number[] = [];
   selecoes = new Set<number>();
+  selecao: any = {};
   private readonly service = inject(AuthService);
   private readonly notify = inject(NzNotificationService);
 
@@ -54,25 +55,20 @@ export class LoginComponent {
 
   selecionar(i: number) {
     this.selecoes.has(i) ? this.selecoes.delete(i) : this.selecoes.add(i);
-  }
-
-  selecionado(i: number) {
-    return this.selecoes.has(i);
-  }
-
-  selecaoContem(i: number) {
-    return [...this.selecoes].includes(i);
+    if (this.selecao[i]) delete this.selecao[i];
+    else this.selecao[i] = i;
   }
 
   limparSelecao() {
     this.selecoes.clear();
+    this.selecao = {};
   }
 
   entrar() {
     this.service.login([...this.selecoes].join('')).subscribe({
       error: (response) => {
         console.error(response);
-        this.notify.error(LBL_ERRO,'Senha invalida!');
+        this.notify.error(LBL_ERRO, 'Senha invalida!');
       },
     });
   }
