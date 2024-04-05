f5f3c3431afdfb6a8b14c74b4730585888b90e62
amsraimar <amsraimar@gmail.com>
2024-04-05 12:11:27 -0300

    Grupo com edicao em form drawer

---
+++ src/app/pages/grupos/grupos-form.component.ts
@@ -0,0 +1,113 @@
+import { Component, inject, input, output } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { EMPTY, catchError, finalize, iif, mergeMap, of } from 'rxjs';
+import {
+  LBL_ATUALIZACAO,
+  LBL_ERRO,
+  MSG_ATUALIZADO_SUCESSO,
+} from '../../common/constantes';
+import { Grupo } from '../../model';
+import { v1 } from 'uuid';
+import { GrupoService } from '../../services/grupo.service';
+import { validarFormulario } from '../../common/util';
+
+const KEY_NOFITY_SALVAR = v1().toString();
+
+@Component({
+  selector: 'app-grupos-form-component',
+  templateUrl: './grupos-form.component.html',
+})
+export class GrupoFormComponent {
+  private readonly service = inject(GrupoService);
+  private readonly notify = inject(NzNotificationService);
+  private readonly formBuilder = inject(FormBuilder);
+
+  visible = input.required<boolean>();
+  visibleChange = output<boolean>();
+  isConfirmLoading = false;
+  data = input.required<Grupo>();
+
+  form = this.formBuilder.group({
+    _id: [''],
+    nome: [
+      '',
+      [Validators.required, Validators.minLength(5), Validators.maxLength(25)],
+    ],
+    principal: [false, Validators.required],
+    multiplo: [false, Validators.required],
+    observacao: ['', Validators.maxLength(100)],
+    cor: [''],
+  });
+
+  ngOnInit() {
+    this.form.setValue({
+      _id: this.data()._id || '',
+      nome: this.data().nome! || '',
+      principal: this.data().principal || false,
+      multiplo: this.data().multiplo || false,
+      observacao: this.data().observacao || '',
+      cor: this.data().cor || '',
+    });
+  }
+
+  salvar() {
+    if (!this.form.valid) {
+      validarFormulario(this.form);
+      return;
+    }
+
+    const data = this.form.value;
+
+    this.isConfirmLoading = true;
+
+    of(this.form.value._id)
+      .pipe(
+        mergeMap((value) =>
+          iif(
+            () => !value,
+            this.service.inlcluir({
+              nome: data.nome!,
+              principal: data.principal!,
+              multiplo: data.multiplo!,
+              observacao: data.observacao || '',
+              cor: data.cor || '',
+            }),
+            this.service.atualizar({
+              id: value!,
+              nome: data.nome!,
+              principal: data.principal!,
+              multiplo: data.multiplo!,
+              observacao: data.observacao || '',
+              cor: data.cor || '',
+            }),
+          ),
+        ),
+        catchError((error: any) => {
+          console.error(error);
+          this.notify.error(LBL_ERRO, error.message);
+          this.isConfirmLoading = false;
+          return EMPTY;
+        }),
+        finalize(() => {
+          this.form.setValue({
+            _id: '',
+            nome: '',
+            principal: false,
+            multiplo: false,
+            observacao: '',
+            cor: '',
+          });
+          this.isConfirmLoading = false;
+        }),
+      )
+      .subscribe({
+        next: () => {
+          this.notify.success(LBL_ATUALIZACAO, MSG_ATUALIZADO_SUCESSO, {
+            nzKey: KEY_NOFITY_SALVAR,
+          });
+          this.visibleChange.emit(false);
+        },
+      });
+  }
+}
--- src/app/pages/grupos/grupos.component.ts
+++ src/app/pages/grupos/grupos.component.ts
@@ -1,28 +1,13 @@
-import { v1 } from 'uuid';
-import { Component, DestroyRef, Signal, computed, inject, signal } from '@angular/core';
-import {
-  EMPTY,
-  Observable,
-  catchError,
-  finalize,
-  iif,
-  mergeMap,
-  of,
-} from 'rxjs'; 
+import { Component, inject, signal } from '@angular/core';
+import { Observable } from 'rxjs';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
 import {
   MSG_EXCLUIR_SUCESSO,
-  MSG_ATUALIZADO_SUCESSO,
-  LBL_ATUALIZACAO,
   LBL_EXCLUSAO,
   LBL_ERRO,
 } from '../../common/constantes';
 import { GrupoService } from '../../services/grupo.service';
 import { Grupo } from '../../model/grupo';
-import { NonNullableFormBuilder, Validators } from '@angular/forms';
-import { validarFormulario } from '../../common/util';
-
-const KEY_NOFITY_SALVAR = v1().toString();
 
 @Component({
   selector: 'app-grupos-component',
@@ -39,122 +24,35 @@ const KEY_NOFITY_SALVAR = v1().toString();
 export class GrupoComponent {
   private readonly service = inject(GrupoService);
   private readonly notify = inject(NzNotificationService);
-  protected readonly destroyRef = inject(DestroyRef);
-  private readonly fb = inject(NonNullableFormBuilder);
 
   data$!: Observable<any[]>;
   loading = signal(false);
-  loadingBtn = false;
 
-  validateForm = this.fb.group({
-    _id: [''],
-    nome: [
-      '',
-      [Validators.required, Validators.minLength(5), Validators.maxLength(25)],
-    ],
-    principal: [false, Validators.required],
-    multiplo: [false, Validators.required],
-    observacao: ['', Validators.maxLength(100)],
-    cor: [''],
-  });
-
-  isVisible = false;
+  editarFormData = signal<any>(null);
+  editarForm = false;
 
   constructor() {
     this.data$ = this.service.data$;
   }
 
-  editar(item: Grupo) {
-    this.validateForm.setValue({
-      _id: item._id,
-      nome: item.nome!,
-      principal: item.principal,
-      multiplo: item.multiplo || false,
-      observacao: item.observacao || '',
-      cor: item.cor || '',
-    });
-    this.isVisible = true;
+  incluir() {
+    this.editar();
+  }
+
+  editar(item?: Grupo) {
+    this.editarFormData.set({ ...item });
+    this.editarForm = true;
   }
 
   remover(item: Grupo) {
-    this.loadingBtn = true;
-    this.service
-      .delete(item._id!)
-      .pipe(
-        finalize(() => {
-          this.loadingBtn = false;
-        }),
-      )
-      .subscribe({
-        error: (error) => {
-          console.error(error);
-          this.notify.error(LBL_ERRO, error.message);
-        },
-        next: () => {
-          this.notify.success(LBL_EXCLUSAO, MSG_EXCLUIR_SUCESSO);
-        },
-      });
-  }
-
-  salvar() {
-    if (!this.validateForm.valid) {
-      validarFormulario(this.validateForm);
-      return;
-    }
-
-    this.loadingBtn = true;
-
-    const data = this.validateForm.value;
-
-    of(this.validateForm.value._id)
-      .pipe(
-        mergeMap((value) =>
-          iif(
-            () => !value,
-            this.service.inlcluir({
-              nome: data.nome!,
-              principal: data.principal!,
-              multiplo: data.multiplo!,
-              observacao: data.observacao,
-              cor: data.cor,
-            }),
-            this.service.atualizar({
-              id: value!,
-              nome: data.nome!,
-              principal: data.principal!,
-              multiplo: data.multiplo!,
-              observacao: data.observacao,
-              cor: data.cor,
-            }),
-          ),
-        ),
-        catchError((error: any) => {
-          console.error(error);
-          this.notify.error('Erro', error.message);
-          return EMPTY;
-        }),
-        finalize(() => {
-          this.loadingBtn = false;
-
-          this.validateForm.setValue({
-            _id: '',
-            nome: '',
-            principal: false,
-            multiplo: false,
-            observacao: '',
-            cor: '',
-          });
-
-          this.isVisible = false;
-        }),
-      )
-      .subscribe({
-        next: () => {
-          this.service.updateData();
-          this.notify.success(LBL_ATUALIZACAO, MSG_ATUALIZADO_SUCESSO, {
-            nzKey: KEY_NOFITY_SALVAR,
-          });
-        },
-      });
+    this.service.delete(item._id!).subscribe({
+      error: (error) => {
+        console.error(error);
+        this.notify.error(LBL_ERRO, error.message);
+      },
+      next: () => {
+        this.notify.success(LBL_EXCLUSAO, MSG_EXCLUIR_SUCESSO);
+      },
+    });
   }
 }
