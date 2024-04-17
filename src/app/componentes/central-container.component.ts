007017e92a92143d63104247ba4ded04e5bd8f7b
amsraimar <amsraimar@gmail.com>
2024-04-17 11:05:12 -0300

    Removendo header dos dependentes, header inferior com fab

--- src/app/componentes/central-container.component.ts
+++ src/app/componentes/central-container.component.ts
@@ -1,4 +1,6 @@
 import { Component, input, output } from '@angular/core';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
@@ -6,10 +8,14 @@ import { MatSidenavModule } from '@angular/material/sidenav';
   selector: 'central-container',
   templateUrl: './central-container.component.html',
   standalone: true,
-  imports: [MatSidenavModule, MatProgressBarModule],
+  imports: [MatSidenavModule, MatProgressBarModule, MatButtonModule, MatIconModule],
 })
 export class CentralContainerComponent {
   loading = input(false);
   drawer = input(false);
   drawerChange = output<boolean>();
+
+  incluir = output<void>();
+
+  hideHeader = input(false);
 }
