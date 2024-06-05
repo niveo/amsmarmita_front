fc99b41f7cd8996b137838bc74c210869284d06b
amsraimar <amsraimar@gmail.com>
2024-06-05 15:18:41 -0300

    Ordenar Marmitas lancadas por lancamento

--- src/app/pages/marmitas/marmitas.component.ts
+++ src/app/pages/marmitas/marmitas.component.ts
@@ -1,8 +1,9 @@
 import { Component, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { MarmitaService } from '@navegador/services/marmita.service';
 import { Marmita } from '@navegador/model/marmita';
 import { BaseContainerComponent } from '@navegador/componentes/base-container.component';
+import { isSameMonth, isSameYear, parseISO, isAfter, isBefore } from 'date-fns';
 
 @Component({
   selector: 'app-marmitas-component',
@@ -11,5 +12,24 @@ import { BaseContainerComponent } from '@navegador/componentes/base-container.co
 })
 export class MarmitasComponent extends BaseContainerComponent<Marmita> {
   override readonly service = inject(MarmitaService);
-  data$: Observable<Marmita[]> = this.service!.data$;
+
+  atual = new Date();
+
+  sortDate(a: Marmita, b: Marmita) {
+    let aLancamento = parseISO(String(a.lancamento));
+    let bLancamento = parseISO(String(b.lancamento));
+    if (
+      isSameMonth(aLancamento, this.atual) &&
+      isSameYear(aLancamento, this.atual)
+    )
+      return -1;
+
+    if (isAfter(aLancamento, bLancamento)) return -1;
+    if (isBefore(aLancamento, bLancamento)) return 1;
+    return 0;
+  }
+
+  data$: Observable<Marmita[]> = this.service!.data$.pipe(
+    map((m) => m.sort((a, b) => this.sortDate(a, b))),
+  );
 }
